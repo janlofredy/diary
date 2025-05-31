cc048df0502812409d6d3e774701ecedb3c35ae2
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2025-05-31 14:17:05 +0000

    add folders, PWA, Element +, supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  modules: [
+    '@vite-pwa/nuxt',
+    '@nuxtjs/supabase',
+    '@element-plus/nuxt'
+  ],
+  pwa: {
+
+  }
 });
