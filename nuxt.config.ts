696b33307b052d87ae04737d9ed61713ee3bbecd
Jose Janlofre Dy <janlofredy@gmail.com>
2025-06-25 19:23:02 +0800

    add tailwind and supabase back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase'
+  ]
 })
