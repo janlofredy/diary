1448083d8d97b6eb1f7cba4160d7276ae2253a04
Jose Janlofre Dy <janlofredy@gmail.com>
2025-06-25 15:53:53 +0800

    move back data from backup

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <NuxtLoadingIndicator />
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
 </template>
